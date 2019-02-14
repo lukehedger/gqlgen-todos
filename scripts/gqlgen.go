1d79e8d82fc63509ca5995c8d197c942547dee13
lukehedger <luke.hedger@gmail.com>
2019-02-14 13:32:34 +0000

    Update config to always generate temporary resolver stubs

--- scripts/gqlgen.go
+++ scripts/gqlgen.go
@@ -2,8 +2,20 @@
 
 package main
 
-import "github.com/99designs/gqlgen/cmd"
+import (
+	"fmt"
+	"os"
+
+	"github.com/99designs/gqlgen/cmd"
+)
 
 func main() {
+	// Remove `tmp/resolver.go` file before generating GraphQL resolvers
+	err := os.Remove("tmp/resolver.go")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
 	cmd.Execute()
 }
